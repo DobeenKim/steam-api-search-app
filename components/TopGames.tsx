1c3a0e37c3395aedbcd423aab8a727f986e0338d
Dobeen Kim <dobin7955@gmail.com>
2026-05-11 13:00:04 +0200

    removed test code

--- components/TopGames.tsx
+++ components/TopGames.tsx
@@ -32,17 +32,6 @@ const TopGames = () => {
         getGames()
     },[])
 
-    const Item = styled(Paper)(({ theme }) => ({
-        backgroundColor: '#fff',
-        ...theme.typography.body2,
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: (theme.vars ?? theme).palette.text.secondary,
-        ...theme.applyStyles('dark', {
-            backgroundColor: '#1A2027',
-        }),
-    }));
-
     return (
         <>
             <Box sx={{
